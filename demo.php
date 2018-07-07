5dc8e8c2a03d2b010863e0425c97da31b4c4dbc2
Isaac <isaacbotwe7@gmail.com>
2018-07-07 10:12:45 +0000

    modified:   Class.Address.inc 	new file:   Class.AddressBusiness.inc 	new file:   Class.AddressPark.inc 	new file:   Class.AddressResidence.inc 	new file:   Class.Model.inc 	modified:   demo.php

--- demo.php
+++ demo.php
@@ -1,10 +1,11 @@
 <?php
 require 'Class.Address.inc';
 require 'Class.Database.inc';
+require 'Class.AddressResidence.inc';
+require 'Class.AddressBusiness.inc';
 
 echo '<h2>Instantiating Address</h2>';
-$address = new Address;
-
+$address = new AddressResidence;
 
 
 echo '<h2>Setting properties...</h2>';
@@ -12,17 +13,18 @@ $address->streetName_1 = '555 Fake Street';
 $address->city_name = 'Townsville';
 $address->subdivision_name = 'State';
 $address->country_name = 'United States of America';
-$address->address_type_id = 1;
 echo $address;
+echo '<tt><pre>'.var_export($address, TRUE).'</pre></tt>';
 
 
 echo '<h2>Testing Address __ construct with an array</h2>';
-$address_2 = new Address(array(
+$address_2 = new AddressBusiness(array(
     'streetName_1' =>'123 Phony Ave',
     'city_name' => 'Villageland',
     'subdivision_name' => 'Region',
     'country_name' => 'Canada',
 ));
 echo $address_2;
+echo '<tt><pre>'.var_export($address_2, TRUE).'</pre></tt>';
 
 ?>
